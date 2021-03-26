511fbf1c3b50061609169217a4bf013a04a52f03
m-kassimi <mhd.kassimi@gmail.com>
2021-03-26 02:12:31 +0100

    mka modification conf et import firebase module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 import { environment } from "src/environments/environment";
 import { NgModule } from '@angular/core';
@@ -8,7 +9,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularFireModule } from '@angular/fire/firebase.app.module';
+
 
 @NgModule({
   declarations: [
